52d5829b590af89518a4e0aecac257a291290cd4
Vivek Goyal <vivekgoyalbhl@gmail.com>
2016-04-03 16:10:36 +0530

    added folder

---
+++ js/modal.js
@@ -0,0 +1,29 @@
+
+// Get the modal
+var modal = document.getElementById('contactDone');
+
+// Get the button that opens the modal
+//var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+//btn.onclick = function() {
+//    modal.style.display = "block";
+//}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+    $('#ss-submit')[0].value='submit';
+    }
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+        $('#ss-submit')[0].value='submit';
+    }
+}
